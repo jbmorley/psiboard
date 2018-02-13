2aa4c44725c27814bbfd30781ab97c07029c0159
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-02-12 21:18:11 -0800

    General key scanning now works.

--- keyboard.py
+++ keyboard.py
@@ -1,43 +1,30 @@
 #!/usr/bin/env python3
 
+import time
+
 import RPi.GPIO as GPIO
 
 
-BUS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23]
-COLUMNS = [:11]
-ROWS = [11:]
-
+COLUMNS = [25, 0, 1, 5, 6, 12, 13, 19, 21]
+ROWS = [22, 23, 24, 10, 9, 11, 8, 7, 16, 26, 20]
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(COLUMNS[0], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COLUMNS[1], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COLUMNS[2], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COLUMNS[3], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COLUMNS[4], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Does nothing?
-GPIO.setup(COLUMNS[5], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Column 7
-GPIO.setup(COLUMNS[6], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # This is column 6
-GPIO.setup(COLUMNS[7], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Column 5
-GPIO.setup(COLUMNS[8], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # DOWN high seems to be column 4
-GPIO.setup(COLUMNS[9], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # DOWN high seems to be column 3
-GPIO.setup(COLUMNS[10], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # DOWN high seems to be column 2
-GPIO.setup(COLUMNS[11], GPIO.IN, pull_up_down=GPIO.PUD_UP) # DOWN high seems to be column?????
+for column in COLUMNS:
+    print("Configuring BCM %d..." % column)
+    GPIO.setup(column, GPIO.OUT)
 
 for row in ROWS:
+    print("Configuring BCM %d..." % row)
     GPIO.setup(row, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     output = ""
-    for pin in COLUMNS + ROWS:
-        output = output + ("%s" % GPIO.input(pin))
+    for column in COLUMNS:
+        GPIO.output(column, 1)
+        time.sleep(0.05)
+        for row in ROWS:
+            output = output + ("%s" % GPIO.input(row))
+        GPIO.output(column, 0)
     print(output)
-
-# while True:
-#     output = ""
-#     for column in COLUMNS:
-#         GPIO.output(column, 1)
-
-#         for row in ROWS:
-#             output = output + ("%s" % GPIO.input(row))
-#         GPIO.output(column, 0)
-#     print(output)
+    exit()
