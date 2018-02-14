ae85506199f056db099d326c2f7224f56940c20b
Jason Barrie Morley <jbmorley@mac.com>
2018-02-14 07:00:03 +0000

    Rudimentary shifting support.

--- keyboard.py
+++ keyboard.py
@@ -11,6 +11,7 @@ ROWS = [22, 23, 24, 10, 9, 11, 8, 7, 16, 26, 20]
 
 
 state = [None, None, None, None, None, None, None, None, None, None, None]
+shift = False
 
 
 def character(column, row):
@@ -18,17 +19,24 @@ def character(column, row):
             [" ",  "x",  "j",  "a",  "2",  "i",  "w",  "8",  None, None, None],
             [None, "c",  "k",  "s",  "3",  "o",  "e",  "9",  None, None, None],
             [",",  "v",  "m",  "d",  "4",  "p",  "r",  "0",  None, None, None],
-            [None, "b",  ".",  "f",  "5",  "l",  "t",  None, None, None, None],
+            [None, "b",  ".",  "f",  "5",  "l",  "t",  "\b \b", None, None, None],
             [None, "n",  None, "g",  "6",  "\n", "y",  "~",  None, None, None],
-            [None, None, None, None, None, None, None, None, None, None, None],
+            [0x01, None, None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None, None, None]][column][row]
 
 
+
 def event(character, down):
+    global shift
     if character is None:
         return
+    if character is 0x01:
+        shift = down
+        return
     if down:
+        if shift:
+            character = character.upper()
         sys.stdout.write(character)
         sys.stdout.flush()
 
@@ -47,7 +55,6 @@ while True:
     for column_index, column in enumerate(COLUMNS):
         output = []
         GPIO.output(column, 1)
-        time.sleep(0.01)
         for row in ROWS:
             output.append(GPIO.input(row))
         GPIO.output(column, 0)
