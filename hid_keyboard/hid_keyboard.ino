be4879d65848fabc81a33bd618bebfa30646d6c8
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-02-25 06:38:27 -0800

    Remove another defunct row.

--- hid_keyboard/hid_keyboard.ino
+++ hid_keyboard/hid_keyboard.ino
@@ -31,20 +31,20 @@ int ROWS[] = {PIN_A1, PIN_A2, PIN_A5, 13, 8, 15, 7, 11};
 #define KEY_NONE HID_KEY_A
 
 #define COL_COUNT 11
-#define ROW_COUNT 9 // TODO: This is now unnecessarily long.
+#define ROW_COUNT 8
 
 static uint8_t CHARACTER_MAP[COL_COUNT][ROW_COUNT] = {
-  { KEY_NONE,            HID_KEY_Z,           HID_KEY_H,          HID_KEY_TAB, HID_KEY_1, HID_KEY_U,      HID_KEY_Q, HID_KEY_7,          KEY_NONE },
-  { HID_KEY_SPACE,       HID_KEY_X,           HID_KEY_J,          HID_KEY_A,   HID_KEY_2, HID_KEY_I,      HID_KEY_W, HID_KEY_8,          KEY_NONE },
-  { HID_KEY_ARROW_UP,    HID_KEY_C,           HID_KEY_K,          HID_KEY_S,   HID_KEY_3, HID_KEY_O,      HID_KEY_E, HID_KEY_9,          KEY_NONE },
-  { HID_KEY_COMMA,       HID_KEY_V,           HID_KEY_M,          HID_KEY_D,   HID_KEY_4, HID_KEY_P,      HID_KEY_R, HID_KEY_0,          KEY_NONE },
-  { HID_KEY_ARROW_LEFT,  HID_KEY_B,           HID_KEY_PERIOD,     HID_KEY_F,   HID_KEY_5, HID_KEY_L,      HID_KEY_T, HID_KEY_BACKSPACE,  KEY_NONE },
-  { HID_KEY_ARROW_RIGHT, HID_KEY_N,           HID_KEY_ARROW_DOWN, HID_KEY_G,   HID_KEY_6, HID_KEY_RETURN, HID_KEY_Y, HID_KEY_APOSTROPHE, KEY_NONE },
-  { HID_KEY_SHIFT_LEFT,  KEY_NONE,            KEY_NONE,           KEY_NONE,    KEY_NONE,  KEY_NONE,       KEY_NONE,  KEY_NONE,           KEY_NONE },
-  { KEY_NONE,            HID_KEY_SHIFT_RIGHT, KEY_NONE,           KEY_CTRL,    KEY_NONE,  KEY_NONE,       KEY_NONE,  KEY_NONE,           KEY_NONE },
-  { KEY_NONE,            KEY_NONE,            KEY_FN,             KEY_NONE,    KEY_NONE,  KEY_NONE,       KEY_NONE,  KEY_NONE,           KEY_NONE },
-  { KEY_NONE,            KEY_NONE,            KEY_NONE,           KEY_NONE,    KEY_NONE,  KEY_MENU,       KEY_NONE,  KEY_NONE,           KEY_NONE },
-  { KEY_NONE,            KEY_NONE,            KEY_NONE,           KEY_NONE,    KEY_NONE,  KEY_MENU,       KEY_ESC,   KEY_NONE,           KEY_NONE },
+  { KEY_NONE,            HID_KEY_Z,           HID_KEY_H,          HID_KEY_TAB, HID_KEY_1, HID_KEY_U,      HID_KEY_Q, HID_KEY_7           },
+  { HID_KEY_SPACE,       HID_KEY_X,           HID_KEY_J,          HID_KEY_A,   HID_KEY_2, HID_KEY_I,      HID_KEY_W, HID_KEY_8,          },
+  { HID_KEY_ARROW_UP,    HID_KEY_C,           HID_KEY_K,          HID_KEY_S,   HID_KEY_3, HID_KEY_O,      HID_KEY_E, HID_KEY_9,          },
+  { HID_KEY_COMMA,       HID_KEY_V,           HID_KEY_M,          HID_KEY_D,   HID_KEY_4, HID_KEY_P,      HID_KEY_R, HID_KEY_0,          },
+  { HID_KEY_ARROW_LEFT,  HID_KEY_B,           HID_KEY_PERIOD,     HID_KEY_F,   HID_KEY_5, HID_KEY_L,      HID_KEY_T, HID_KEY_BACKSPACE,  },
+  { HID_KEY_ARROW_RIGHT, HID_KEY_N,           HID_KEY_ARROW_DOWN, HID_KEY_G,   HID_KEY_6, HID_KEY_RETURN, HID_KEY_Y, HID_KEY_APOSTROPHE, },
+  { HID_KEY_SHIFT_LEFT,  KEY_NONE,            KEY_NONE,           KEY_NONE,    KEY_NONE,  KEY_NONE,       KEY_NONE,  KEY_NONE,           },
+  { KEY_NONE,            HID_KEY_SHIFT_RIGHT, KEY_NONE,           KEY_CTRL,    KEY_NONE,  KEY_NONE,       KEY_NONE,  KEY_NONE,           },
+  { KEY_NONE,            KEY_NONE,            KEY_FN,             KEY_NONE,    KEY_NONE,  KEY_NONE,       KEY_NONE,  KEY_NONE,           },
+  { KEY_NONE,            KEY_NONE,            KEY_NONE,           KEY_NONE,    KEY_NONE,  KEY_MENU,       KEY_NONE,  KEY_NONE,           },
+  { KEY_NONE,            KEY_NONE,            KEY_NONE,           KEY_NONE,    KEY_NONE,  KEY_MENU,       KEY_ESC,   KEY_NONE,           },
 };
 
 struct ModifierBehavior {
