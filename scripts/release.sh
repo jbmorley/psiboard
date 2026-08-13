32711da8340dd785d35c418fa58594ec082b8a73
Jason Morley <hello@jbmorley.co.uk>
2026-08-12 16:05:07 -1000

    docs: ©️ Update copyright to 2026

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2018-2026 Jason Morley
+# Copyright (c) 2016-2026 Jason Morley
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
